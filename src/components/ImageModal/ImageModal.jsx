d4c2a37da330450664c066955639e764199af7e6
pavsoldatov <snowschoe@gmail.com>
2022-10-15 15:26:44 +0300

    Close modal on Escape

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
 import CloseButton from "./CloseButton";
 import Backdrop from "./Backdrop";
@@ -7,6 +8,14 @@ import "./ImageModal.scss";
 const ImageModal = ({ setViewed, viewed }) => {
   const { isViewed, viewId } = viewed;
   const src = `https://picsum.photos/id/${viewId}/1940/1200`;
+
+  useEffect(() => {
+    const close = (e) => {
+      if (e.key === "Escape") setViewed(false);
+    };
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
 
   return (
     isViewed && (
